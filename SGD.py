7b5f63e73005592f485112849b5f3c93fc992398
Chenhao Li <chenhal5@uci.edu>
2019-11-17 14:40:50 -0800

    Create SGD.py

---
+++ SGD.py
@@ -0,0 +1,14 @@
+import numpy as np
+from methods import gradient
+
+
+class SGDOpt():
+
+    def __init__(self):
+        pass
+
+    def step(self):
+        pass
+
+    def optimize(self):
+        pass
